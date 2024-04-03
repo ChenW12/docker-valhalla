49fdd0ff7ba3a1784f946157fd425c15738851af
ZM_L <614292478@qq.com>
2024-04-03 12:50:05 +0100

    Adding scripts for testing

---
+++ tests/send_request.sh
@@ -0,0 +1,13 @@
+r='{"locations":[{"lat":'
+r+=$1
+r+=',"lon":'
+r+=$2
+r+='},{"lat":'
+r+=$3
+r+=',"lon":'
+r+=$4
+r+='}],"costing":"pedestrian","directions_options":{"units":"miles"}}'
+#echo "$r"
+curl localhost:8002/route -d $r
+
+
